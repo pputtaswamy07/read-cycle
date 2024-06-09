e82ddb1e28c8cb2e63feec45dac2c16cad06e3bf
Poojaputtaswamy <poojaputtaswamy07@gmail.com>
2024-06-09 20:18:47 +0200

    login-page component is created and added in navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { AddBookComponent } from './book-details/add-book.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'bookList', component: BookListComponent },
   { path: 'addBook', component: AddBookComponent },
+  { path: 'loginPage', component: LoginPageComponent },
 ];
 
 @NgModule({
