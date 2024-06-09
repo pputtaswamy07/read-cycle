09eebc428bc7ea8419256b6d2973409193fce0fe
Poojaputtaswamy <poojaputtaswamy07@gmail.com>
2024-06-09 22:08:41 +0200

    added signup and login routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { AddBookComponent } from './book-details/add-book.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'bookList', component: BookListComponent },
   { path: 'addBook', component: AddBookComponent },
   { path: 'loginPage', component: LoginPageComponent },
+  { path: 'signupPage', component: SignupPageComponent },
 ];
 
 @NgModule({
