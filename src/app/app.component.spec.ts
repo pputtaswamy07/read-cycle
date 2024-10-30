9f38560b2128743cb531103ee19c1fbe189642bf
Poojaputtaswamy <poojaputtaswamy07@gmail.com>
2024-10-30 16:50:51 +0100

    update login page

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,15 @@ describe('AppComponent', () => {
   it(`should have the 'read-cycle' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('read-cycle');
+    expect(app['title']).toEqual('read-cycle');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, read-cycle');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, read-cycle'
+    );
   });
 });
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,29 +1,41 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
-  selector: 'app-login-page',
-  standalone: true,
-  imports: [FormsModule, CommonModule, RouterLink],
+  selector: 'app-login',
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent {
-  email: string | undefined;
-  password: string | undefined;
-  signupEmail: string | undefined;
-  signupPassword: string | undefined;
-  isSignUpVisible: boolean = false;
+  [x: string]: any;
+  fb = inject(FormBuilder);
+  http = inject(HttpClient);
+  router = inject(Router);
+  authService = inject(AuthService);
+  form = this.fb.nonNullable.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+  errorMessage: string | null = null;
 
-  constructor(private router: Router) {}
-
-  onSubmit() {
-    // Handle login logic here
-    console.log('Login:', this.email, this.password);
+  onSubmit(): void {
+    const rawForm = this.form.getRawValue();
+    this.authService.login(rawForm.email, rawForm.password).subscribe({
+      next: () => {
+        this.router.navigateByUrl('/home');
+      },
+      error: (err) => {
+        this.errorMessage = err.code;
+      },
+    });
   }
+
   navigateToSignUp() {
-    this.router.navigate(['/signup']);
+    this.router.navigateByUrl('/signup');
   }
 }
