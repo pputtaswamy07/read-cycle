3f74efdb9d96ee2dfa2fba34b820b5d7b30fb24a
Poojaputtaswamy <poojaputtaswamy07@gmail.com>
2024-10-30 12:41:23 +0100

    update login page

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDpRVC3qZ6RUMTqru_Wx3QtLsHfiHBQQ-E',
+    authDomain: 'read-cycle-2692d.firebaseapp.com',
+    projectId: 'read-cycle-2692d',
+    storageBucket: 'read-cycle-2692d.appspot.com',
+    messagingSenderId: '74354895481',
+    appId: '1:74354895481:web:ab99d045266b2dfd0d42a8',
+    measurementId: 'G-9YS441YT4T',
+  },
+};
