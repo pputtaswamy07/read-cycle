01785645c164979889beb5bf78ad77242e63fa33
Poojaputtaswamy <poojaputtaswamy07@gmail.com>
2024-10-30 16:50:13 +0100

    create auth service module

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { Observable, from } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  constructor(private firebaseAuth: Auth) {}
+
+  register(
+    email: string,
+    username: string,
+    password: string
+  ): Observable<void> {
+    const promise = createUserWithEmailAndPassword(
+      this.firebaseAuth,
+      email,
+      password
+    ).then((response) => {
+      return updateProfile(response.user, { displayName: username });
+    });
+
+    return from(promise);
+  }
+
+  login(email: string, password: string): Observable<void> {
+    const promise = signInWithEmailAndPassword(
+      this.firebaseAuth,
+      email,
+      password
+    ).then(() => {});
+    return from(promise);
+  }
 }
